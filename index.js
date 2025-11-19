3ded42f9f6b211b75534427cc6a5b586bdb9d664
Jose Carlos Velasco <jcvl0611@gmail.com>
2025-11-19 10:34:41 -0600

    feat: FizzBuzz Logic added

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 // FizzBuzz Javascript
 //1. START
 for(let i=1;i<=1000; i++){ //Initialize Loop
-console.log(i); //Just for debugging
+//2.1 DECISION Is i divisible by 15?   
+if(!(i%15)){  //Checks if i can be divided by 15
+//If yes
+console.log("FizzBuzz"); //3.1 OUTPUT. Prints FizzBuzz
+}
+//If not
+//2.2 DECISION Is i divisible by 5?
+else if(!(i%5)){ //Checks if i can be divided by 5
+console.log("Buzz"); //3.2 OUTPUT. Prints Buzz
+}
+//If not
+//2.3 DECISION Is i divisible by 3?
+else if(!(i%3)){ //Checks if i can be divided by 3
+console.log("Fizz"); //3.3 OUTPUT. Prints Fizz
+}
+//Otherwise
+else{ //The number is not divisible by 15,5 or 3
+    console.log(i); //3.4 OUTPUT. Prints i
+}
+//4. END
 }
